b1b2f65a5801b50c2dbc26ce86609c5d97adf2fb
DarrylSSY <ssy.darryl@gmail.com>
2023-03-21 03:55:51 +0800

    Create prefabs for pouring

---
+++ Assets/PouringScene/PourDetector.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 30;
+    public GameObject streamPrefab = null;
+
+    private bool isPouring = false;
+
+    private void Update(){
+        print(CalculatePourAngle());
+        bool pourCheck = CalculatePourAngle() > pourThreshold;
+        if (isPouring != pourCheck){
+            isPouring = pourCheck;
+            if (isPouring){
+                StartPour();
+            }
+            else{
+                EndPour();
+            }
+        }
+    }
+
+    private void StartPour(){
+        print("Start");
+        // create streamPrefab object at origin
+        // streamPrefab has to be a child
+        // of the object this script is attached to
+        // so that it rotates with the object
+        // and the stream is always facing the camera
+
+        var newObject = Instantiate(streamPrefab);
+        newObject.transform.parent = transform;
+        newObject.transform.localPosition = Vector3.zero;
+        
+        
+
+    }
+
+    private void EndPour(){
+        print("End");
+        // destroy streamPrefab object
+        Destroy(transform.GetChild(0).gameObject);
+
+    }
+
+    private float CalculatePourAngle(){
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+}
