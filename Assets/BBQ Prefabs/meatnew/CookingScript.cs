0252bcb808868ed42b816a72253399ee7dd50cab
DarrylSSY <ssy.darryl@gmail.com>
2023-03-30 01:40:06 +0800

    Making cooking physics work with meat

--- Assets/BBQ Prefabs/meatnew/CookingScript.cs
+++ Assets/BBQ Prefabs/meatnew/CookingScript.cs
@@ -12,8 +12,9 @@ public class CookingScript : MonoBehaviour
     // ui object
     public GameObject cookAmount;
     public GameObject burnAmount;
-    
-
+    bool isColliding = false;
+    int cookingStage = 0;
+    public float timer = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,38 +30,46 @@ public class CookingScript : MonoBehaviour
     {
         // change cookAmount's text, to increase the number by 1
         // code begins
+        if (isColliding)
+        {
+            timer += Time.deltaTime;
+            UpdateCookingStage();
+        }
     }
     // When this object touches another object tagged "Stove" for 5 seconds, change the color of the object to red
     // after another 5 seconds, change the color to black
     // code begins
     void OnCollisionEnter(Collision collision)
     {
-        // change meshrenderer's second material to materials[0]
-        
-        
-
-
-
-        print(collision.gameObject.tag);
         if (collision.gameObject.tag == "Stove")
         {
-            StartCoroutine(ChangeColor(cook, 5));
-            StartCoroutine(ChangeColor(burn, 10));
+            // change meshrenderer's second material to materials[0]
+            isColliding = true;
         }
+
     }
 
-    IEnumerator ChangeColor(Material[] materials, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        if (materials == cook)
-        {
-            cookAmount.GetComponent<UnityEngine.UI.Text>().text = (int.Parse(cookAmount.GetComponent<UnityEngine.UI.Text>().text) + 1).ToString();
-        }
-        else
-        {
+    void OnCollisionExit(Collision collision){
+        isColliding = false;
+    }
+
+    void UpdateCookingStage(){
+        if( timer > 10 && cookingStage == 1){
+            cookingStage = 2;
+            ChangeColor(burn);
             burnAmount.GetComponent<UnityEngine.UI.Text>().text = (int.Parse(burnAmount.GetComponent<UnityEngine.UI.Text>().text) + 1).ToString();
             cookAmount.GetComponent<UnityEngine.UI.Text>().text = (int.Parse(cookAmount.GetComponent<UnityEngine.UI.Text>().text) - 1).ToString();
         }
+        else if(timer > 5 && cookingStage == 0){
+            cookingStage = 1;
+            ChangeColor(cook);
+            cookAmount.GetComponent<UnityEngine.UI.Text>().text = (int.Parse(cookAmount.GetComponent<UnityEngine.UI.Text>().text) + 1).ToString();
+        }
+    }
+
+
+    void ChangeColor(Material[] materials)
+    {
         GetComponent<MeshRenderer>().materials = materials;
     }
 
