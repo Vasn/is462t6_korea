b4ba179021f56336a982de2776d48db601dda6e3
Pochienlin <linxpochien@gmail.com>
2023-03-30 22:05:07 +0800

    As the branch title suggests

--- Assets/ChecklistManager.cs
+++ Assets/ChecklistManager.cs
@@ -30,22 +30,35 @@ public class ChecklistManager : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        // make the object a child of the collided object, set rotation to 0 and physics to stop
+        
         if (collision.gameObject.tag == "Ramen")
         {
             ramen = true;
+            collision.gameObject.transform.SetParent(this.transform);
+            collision.gameObject.transform.rotation = Quaternion.identity;
+            collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            collision.gameObject.GetComponent<Collider>().enabled = false;
         }
         if (collision.gameObject.tag == "Soju")
         {
             soju = true;
+            collision.gameObject.transform.SetParent(this.transform);
+            collision.gameObject.transform.rotation = Quaternion.identity;
+            collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            collision.gameObject.GetComponent<Collider>().enabled = false;
         }
         if (collision.gameObject.tag == "Toothpaste")
         {
             toothpaste = true;
+            collision.gameObject.transform.SetParent(this.transform);
+            collision.gameObject.transform.rotation = Quaternion.identity;
+            collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            collision.gameObject.GetComponent<Collider>().enabled = false;
         }
 
-        Destroy(collision.gameObject);
-        //Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Toothpaste: " + toothpaste);
-    // build the chceklist strings
+        Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Toothpaste: " + toothpaste);
+        //  build the chceklist strings
         tobuy = "To buy: \n";
         bought = "\nBought: \n";
         if (soju == true)
@@ -77,5 +90,59 @@ public class ChecklistManager : MonoBehaviour
 
         string compiled = tobuy +"\n"+ bought;
         checklistText.text = compiled.Replace("\n", Environment.NewLine);
+
+        Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Toothpaste: " + toothpaste);
+       
+        
+    }
+
+    public void update_me(GameObject grabbable)
+    {
+        if (grabbable.tag == "Ramen")
+        {
+            ramen = true;
+        }
+        if (grabbable.tag == "Soju")
+        {
+            soju = true;
+        }
+        if (grabbable.tag == "Toothpaste")
+        {
+            toothpaste = true;
+        }
+
+        tobuy = "To buy: \n";
+        bought = "\nBought: \n";
+        if (soju == true)
+        {
+            bought += "1. Soju \n";
+        }
+        else
+        {
+            tobuy += "1. Soju \n";
+        }
+
+        if (ramen == true)
+        {
+            bought += "2. Instant Ramen \n";
+        }
+        else
+        {
+            tobuy += "2. Instant Ramen \n";
+        }
+
+        if (toothpaste == true)
+        {
+            bought += "3. Toothpaste \n";
+        }
+        else
+        {
+            tobuy += "3. Toothpaste \n";
+        }
+
+        string compiled = tobuy +"\n"+ bought;
+        checklistText.text = compiled.Replace("\n", Environment.NewLine);
+
+        Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Toothpaste: " + toothpaste);
     }
 }
