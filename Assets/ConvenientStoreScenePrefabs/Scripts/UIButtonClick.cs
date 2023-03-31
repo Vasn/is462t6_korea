be1a14a564b6e505a8e0c3efe3261612ccdfe83a
sjng1234 <ng.shen.jie1999@gmail.com>
2023-03-31 14:43:52 +0800

    Updated Dialog

--- Assets/ConvenientStoreScenePrefabs/Scripts/UIButtonClick.cs
+++ Assets/ConvenientStoreScenePrefabs/Scripts/UIButtonClick.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIButtonClick : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject DialogBox;
+    public TextMeshProUGUI dialogText;
     public string action;
     private DialogManager dialogManager;
     void Start()
@@ -23,8 +25,25 @@ public class UIButtonClick : MonoBehaviour
             case "next":
                 dialogManager.goToNext();
                 break;
+            case "yesBag":
+                dialogManager.yesBag();
+                break;
+            case "noBag":
+                dialogManager.noBag();
+                break;
             default:
                 break;
         }
+    }
+    public void ChangeText(string text)
+    {
+        // Change the text of the button
+        dialogText.text = text;
+    }
+
+    public void ChangeAction(string action)
+    {
+        // Change the action of the button
+        this.action = action;
     }
 }
