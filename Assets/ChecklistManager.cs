e21903bdf8bd953cfc5e356625b3e4b5298e9f43
Pochienlin <linxpochien@gmail.com>
2023-03-31 13:42:53 +0800

    Basket now follows player
    Basket, now more user friendly!

--- Assets/ChecklistManager.cs
+++ Assets/ChecklistManager.cs
@@ -6,9 +6,10 @@ using System;
 
 public class ChecklistManager : MonoBehaviour
 {
+    public Transform anchor;
     public bool ramen;
     public bool soju;
-    public bool toothpaste;
+    public bool chips;
     public TextMeshProUGUI checklistText;
 
     private string tobuy;
@@ -16,21 +17,22 @@ public class ChecklistManager : MonoBehaviour
 
     public Transform ramenTransform;
     public Transform sojuTransform;
-    public Transform toothpasteTransform;
+    public Transform chipsTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         ramen=false;
         soju=false;
-        toothpaste=false;
+        chips=false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // fix this object's transform to anchor's transform
+        // this.transform.position = anchor.position;
     }
 
     void OnTriggerEnter(Collider other)
@@ -61,104 +63,49 @@ public class ChecklistManager : MonoBehaviour
                 other.gameObject.GetComponent<Collider>().enabled = false;
             }
         }
-        if (other.gameObject.tag == "Toothpaste")
+        if (other.gameObject.tag == "Chips")
         {
-            if(!toothpaste){
-                ramen = true;
+            if(!chips){
+                chips = true;
                 other.gameObject.transform.SetParent(this.transform);
                 other.gameObject.transform.rotation = Quaternion.identity;
-                other.gameObject.transform.position = toothpasteTransform.position;
+                other.gameObject.transform.position = chipsTransform.position;
                 Destroy(other.gameObject.GetComponent<Rigidbody>());
                 other.gameObject.GetComponent<Collider>().enabled = false;
             }
         }
 
-        Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Toothpaste: " + toothpaste);
+        Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Chips: " + chips);
         //  build the chceklist strings
         tobuy = "To buy: \n";
         bought = "\nBought: \n";
-        if (soju == true)
-        {
+        if (soju){
             bought += "1. Soju \n";
-        }
-        else
+        }else
         {
             tobuy += "1. Soju \n";
         }
 
-        if (ramen == true)
-        {
+        if (ramen){
             bought += "2. Instant Ramen \n";
-        }
-        else
+        }else
         {
             tobuy += "2. Instant Ramen \n";
         }
 
-        if (toothpaste == true)
+        if (chips){
+            bought += "3. Chips \n";
+        }else
         {
-            bought += "3. Toothpaste \n";
-        }
-        else
-        {
-            tobuy += "3. Toothpaste \n";
+            tobuy += "3. Chips \n";
         }
 
         string compiled = tobuy +"\n"+ bought;
         checklistText.text = compiled.Replace("\n", Environment.NewLine);
 
-        Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Toothpaste: " + toothpaste);
+        Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Chips: " + chips);
        
         
     }
 
-    public void update_me(GameObject grabbable)
-    {
-        if (grabbable.tag == "Ramen")
-        {
-            ramen = true;
-        }
-        if (grabbable.tag == "Soju")
-        {
-            soju = true;
-        }
-        if (grabbable.tag == "Toothpaste")
-        {
-            toothpaste = true;
-        }
-
-        tobuy = "To buy: \n";
-        bought = "\nBought: \n";
-        if (soju == true)
-        {
-            bought += "1. Soju \n";
-        }
-        else
-        {
-            tobuy += "1. Soju \n";
-        }
-
-        if (ramen == true)
-        {
-            bought += "2. Instant Ramen \n";
-        }
-        else
-        {
-            tobuy += "2. Instant Ramen \n";
-        }
-
-        if (toothpaste == true)
-        {
-            bought += "3. Toothpaste \n";
-        }
-        else
-        {
-            tobuy += "3. Toothpaste \n";
-        }
-
-        string compiled = tobuy +"\n"+ bought;
-        checklistText.text = compiled.Replace("\n", Environment.NewLine);
-
-        Debug.Log("Ramen: " + ramen + " Soju: " + soju + " Toothpaste: " + toothpaste);
-    }
 }
