7da65d7f8c2421f893f1dffb6bf677cd781b1784
DarrylSSY <ssy.darryl@gmail.com>
2023-04-07 19:43:15 +0800

    Add scene manager and score to cooking scene

---
+++ Assets/BBQ Prefabs/BBQSceneManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BBQSceneManager : MonoBehaviour
+{
+    public GameObject firstAmount;
+    public GameObject secondAmount;
+    public GameObject thirdAmount;
+    
+    public GameObject timer;
+    
+    // integer score
+    public int score;
+    
+    // boolean ifWon
+    public bool ifOver = false;
+    // Start is called before the first frame update
+    public GameObject stampManager;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Countdown from 2 minutes
+        timer.GetComponent<UnityEngine.UI.Text>().text = (20 - Time.timeSinceLevelLoad).ToString("F0");
+        if (timer.GetComponent<UnityEngine.UI.Text>().text == "0")
+        {
+            // Game over
+            if (ifOver == false) {
+                AddScore();
+            }
+            ifOver = true;
+
+        }
+    }
+    
+    public void AddScore()
+    {
+        ifOver = true;
+        if (ifOver)
+        {
+            print("ovar!");
+            if (firstAmount.GetComponent<UnityEngine.UI.Text>().text == "0")
+            {
+                score += 1;
+            }
+            if (secondAmount.GetComponent<UnityEngine.UI.Text>().text == "100")
+            {
+                score += 1;
+            }
+            if (thirdAmount.GetComponent<UnityEngine.UI.Text>().text == "100")
+            {
+                score += 1;
+            }
+            {
+                // You lose
+            }
+
+            if (score <= 1)
+            {
+                stampManager.GetComponent<StampManager>().setStars(1);
+            }
+            else if (score == 2)
+            {
+                stampManager.GetComponent<StampManager>().setStars(2);
+            }
+            else if (score == 3)
+            {
+                stampManager.GetComponent<StampManager>().setStars(3);
+            }
+            
+        }
+    }
+    
+    
+}
